b5130a38841badcdee9eeb18e1139ec90b127f20
Hunter Damron <hdamron1594@yahoo.com>
2019-02-15 12:41:36 -0500

    Merged fix on Ripple problem from branch 'ripple'

--- Ripple/solutions/solution.py
+++ Ripple/solutions/solution.py
@@ -5,8 +5,13 @@
 
 from itertools import chain
 
+DEBUG = False
+
 def pworld(world, cancel=True):
     return "\n".join(reversed(["".join(str(int(x%2) if cancel else x%10) for x in row) for row in world]))
+
+def pblocks(world):
+    return "\n".join(reversed(["".join('■' if x%2 == 1 else '□' for x in row) for row in world]))
 
 def main():
     w, h, n = (int(x) for x in input().split())
@@ -21,10 +26,12 @@ def main():
             diagonals = (((x-i, y+dt-i-1), (x+dt-i-1, y+i), (x+i, y-dt+i+1), (x-dt+i+1, y-i)) for i in range(dt-1))
             for x, y in chain(*(diagonals)):
                 world[y % h][x % w] += 1
+        if DEBUG: print("%s\n" % pblocks(world))
 
-    wavesum = sum(x for x in chain(*world) if x % 2 == 1)
+    wavesum = sum(x % 2 for x in chain(*world))
 
-    # print("%s\n\n%s\n" % (pworld(world, cancel=False), pworld(world)))
+    # if DEBUG: print("%s\n\n%s\n" % (pworld(world, cancel=False), pworld(world)))
+    if DEBUG: print(pblocks(world))
     print(wavesum)
 
 if __name__ == "__main__":
--- Ripple/solve_all_tests.sh
+++ Ripple/solve_all_tests.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+make -C solutions/
 for f in input/input*.txt; do
+  echo "Test $(basename $f .txt)"
   of="$(echo $f | sed 's/input/output/g')"
   #of="output/$(basename $f .in).out"
   cat $f | solutions/solution.py > $of
+  ./solution_diff.sh $f
 done
